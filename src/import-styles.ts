2ccf759243f512efec261b26709cd89f19b6b94d
Watercycle <Watercycle@email.com>
2020-11-06 00:37:33 -0600

    Establish convention for root component styles

--- src/import-styles.ts
+++ src/import-styles.ts
@@ -1,6 +1,11 @@
+/**
+ * References:
+ * https://materializecss.com
+ */
+
 // Vendor
 import 'materialize-css/dist/css/materialize.css'
 import 'css-ripple-effect/dist/ripple.css'
 
 // Shared styles
-import './shared-styles//all.styl'
+import './shared-styles/all.styl'
--- src/pages/primary-page/primary-page.tsx
+++ src/pages/primary-page/primary-page.tsx
@@ -5,7 +5,15 @@ import { WcButton } from '../../shared-components/button/button'
 export interface Props {}
 
 export const PrimaryPage: Component<Props> = props =>
-  <>
-    <div class={styles.root}>Hello World!</div>
+  <div class={styles.root}>
+    <div>Hello World!</div>
+
+    <div class="card-panel green">
+      <div class="input-field">
+        <label for="text1" class="active">Text 1</label>
+        <input id="text1" type="text" placeholder="Insert some text here"/>
+      </div>
+    </div>
+
     <WcButton>Some Text</WcButton>
-  </>
+  </div>
